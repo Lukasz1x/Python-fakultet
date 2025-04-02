cea3703755c11c9c179a222b6edaea394102a2a2
Łukasz Bałabuch <lukas.balabuch@gmail.com>
2025-04-02 18:09:41 +0200

    lab6

--- weather/main_widget.py
+++ weather/main_widget.py
@@ -2,6 +2,8 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout, QMessageBox, QListWidget, \
     QListWidgetItem
 import requests
+from settings_dialog import SettingsDialog
+
 
 
 class MainWidget(QWidget):  # dziedziczenie po QWidget
@@ -12,33 +14,33 @@ class MainWidget(QWidget):  # dziedziczenie po QWidget
         # self refers to the current object instance, it is essential for accessing attributes and methods within the class
 
         # tworzymy atrybuty - elementy gui
-        self.label = QLabel(self)  # "etykieta" z temperaturą
-        self.button = QPushButton(self)  # przycisk
-        self.edit = QLineEdit("Lublin", self)  # okienko na wpisanie nazwy miasta
-        self.city_list = QListWidget(self)  # lista wyszukiwań
+        self.label = QLabel(self)                                       # "etykieta" z temperaturą
+        self.button = QPushButton(self)                                 # przycisk
+        self.settings_button=QPushButton("Settings", self)
+        self.edit = QLineEdit("Lublin", self)                           # okienko na wpisanie nazwy miasta
+        self.city_list = QListWidget(self)                              # lista wyszukiwań
 
-        self.button.clicked.connect(
-            self._on_button_clicked)  # po kliknięciu przycisku wywołujemy metodę _on_button_clicked() - connect przyjmuje metodę bez ()
-        self.city_list.itemClicked.connect(
-            self._on_city_clicked)  # po kliknięciu na miasto z listy wyników wywołujemy metodę _on_city_clicked()
+        self.button.clicked.connect(self._on_button_clicked)            # po kliknięciu przycisku wywołujemy metodę _on_button_clicked() - connect przyjmuje metodę bez ()
+        self.city_list.itemClicked.connect(self._on_city_clicked)       # po kliknięciu na miasto z listy wyników wywołujemy metodę _on_city_clicked()
+        self.settings_button.clicked.connect(self.__show_settings)
 
-        layout = QGridLayout(self)  # tworzymy interfejs i jego układ
-        layout.addWidget(self.edit, 0, 0, 1, 1)  # addWidget(element, row, column, rowSpan, columnSpan, [alignment=0])
+        layout = QGridLayout(self)                                      # tworzymy interfejs i jego układ
+        layout.addWidget(self.edit, 0, 0, 1, 1)                         # addWidget(element, row, column, rowSpan, columnSpan, [alignment=0])
         layout.addWidget(self.button, 0, 1, 1, 1)
         layout.addWidget(self.city_list, 1, 0, 1, 2)
         layout.addWidget(self.label, 2, 0, 1, 2)
+        layout.addWidget(self.settings_button, 3, 0, 1, 2)
+
+        self.weather_params = []
 
     # metoda do wyszukiwania miasta
     def _on_button_clicked(self):
         text = self.edit.text()  # pobieramy tekst z okienka
         # self.label.setText(text)
-        response = requests.get(
-            f'https://geocoding-api.open-meteo.com/v1/search?name={text}')  # wysyłamy zapytanie do api pogodowego
-        # print(response.json())
+        response = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={text}')  # wysyłamy zapytanie do api pogodowego
         json = response.json()  # tworzymy słownik
         if "results" not in json.keys():  # jeśli nie ma wyników
-            QMessageBox.information(self, "Błąd",
-                                    "Nie ma takiej miejscowości.")  # wyskakuje okienko z informacją o błędzie
+            QMessageBox.information(self, "Błąd", "Nie ma takiej miejscowości.")  # wyskakuje okienko z informacją o błędzie
             return
 
         # jeśli są wyniki
@@ -54,10 +56,13 @@ class MainWidget(QWidget):  # dziedziczenie po QWidget
 
     # metoda do zwracania wyników dla danego miasta
     def _on_city_clicked(self):
-        latitude, longitude = self.city_list.currentItem().data(
-            Qt.UserRole)  # pobieramy dane z aktualnie wybranego elementu
-        response = requests.get(
-            f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m')  # wysyłamy zapytanie do api z pobranymi współrzędnymi
+        latitude, longitude = self.city_list.currentItem().data(Qt.UserRole)  # pobieramy dane z aktualnie wybranego elementu
+        response = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&{self.weather_params}')  # wysyłamy zapytanie do api z pobranymi współrzędnymi
         json = response.json()  # tworzymy słownik
-        self.label.setText(
-            f"{json["current"]["temperature_2m"]} stopni C")  # pobieramy z sekcji current temperaturę i wyświetlamy
+        self.label.setText(f"{json["current"]}")  # pobieramy z sekcji current temperaturę i wyświetlamy
+
+    def __show_settings(self):
+        settings_dialog = SettingsDialog(self)
+        settings_dialog.exec()
+        if settings_dialog.result():
+            self.weather_params = settings_dialog.get_params()
